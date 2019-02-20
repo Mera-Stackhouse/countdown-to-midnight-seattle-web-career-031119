0af40e27fc0112fd3838a658fcc841e9accbddac
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-02-20 02:08:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,7 @@
 def countdown(seconds)
   while seconds >= 0
     "#{seconds} SECOND(S)!"
+    second -= 1
   end
   "HAPPY NEW YEAR!"
 end
@@ -10,6 +11,7 @@ end
 def countdown_with_sleep
   while seconds >= 0 
     "#{seconds} SECOND(S)!"
+    seconds -= 1
     sleep(1)
   end
   "HAPPY NEW YEAR!"
